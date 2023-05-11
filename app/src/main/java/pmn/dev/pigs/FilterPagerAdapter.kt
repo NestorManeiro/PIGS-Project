9be92c73bd68a1bb1912a2123b915a751af59883
NestorManeiro <91387558+NestorManeiro@users.noreply.github.com>
2023-05-11 15:58:56 +0100

    Filter fixed.

--- app/src/main/java/pmn/dev/pigs/FilterPagerAdapter.kt
+++ app/src/main/java/pmn/dev/pigs/FilterPagerAdapter.kt
@@ -2,14 +2,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.annotation.NonNull
 import androidx.viewpager.widget.PagerAdapter
 import pmn.dev.pigs.MainActivity
 import pmn.dev.pigs.R
+import java.time.LocalDate
+import com.jakewharton.threetenabp.AndroidThreeTen
+import java.time.LocalTime
 
 class FilterPagerAdapter(private val context: Context, private val layouts: List<Int>) :
     PagerAdapter() {
-
     private val views = mutableListOf<View>()
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
@@ -23,14 +27,27 @@ class FilterPagerAdapter(private val context: Context, private val layouts: List
             }
             1 -> {
                 (context as MainActivity).timePickerFilter = layout.findViewById(R.id.time_picker_filter)
+                initializeTimePicker((context as MainActivity).timePickerFilter)
             }
             2 -> {
                 (context as MainActivity).datePickerFilter = layout.findViewById(R.id.date_picker_filter)
+                initializeDatePicker((context as MainActivity).datePickerFilter)
             }
         }
 
         views.add(layout) // Agrega la vista a la lista views
         return layout
+    }
+
+    private fun initializeTimePicker(timePicker: TimePicker) {
+        val currentTime = LocalTime.now()
+        timePicker.hour = currentTime.hour
+        timePicker.minute = currentTime.minute
+    }
+
+    private fun initializeDatePicker(datePicker: DatePicker) {
+        val today = LocalDate.now()
+        datePicker.init(today.year, today.monthValue - 1, today.dayOfMonth, null)
     }
 
 
--- app/src/main/java/pmn/dev/pigs/MainActivity.kt
+++ app/src/main/java/pmn/dev/pigs/MainActivity.kt
@@ -4,19 +4,24 @@ import FilterPagerAdapter
 import TripAdapter
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import com.jakewharton.threetenabp.AndroidThreeTen
 import pmn.dev.pigs.model.Trip
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import kotlin.math.abs
 import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
@@ -35,7 +40,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        AndroidThreeTen.init(application)
         // Inicializar RecyclerView
         recyclerView = findViewById(R.id.trips_recyclerview)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -56,48 +61,43 @@ class MainActivity : AppCompatActivity() {
 
     private fun createSampleTrips(): List<Trip> {
         return listOf(
+            Trip("John Doe", "Universidad", "Centro de la ciudad", "08:00", "11-05-2023"),
+            Trip("Pedro Perez", "Facultad de Informática", "Santa Catalina", "15:00", "11-05-2023"),
+            Trip("Maria Lopez", "Estación de autobuses", "Facultad de Ciencias", "16:00", "11-05-2023"),
             Trip("John Doe", "Universidad", "Centro de la ciudad", "08:00", "12-05-2023"),
-            Trip(
-                "Jane Smith",
-                "Estación de autobuses",
-                "Facultad de Ciencias",
-                "09:00",
-                "12-05-2023"
-            ),
-            Trip(
-                "Andres Rodriguez",
-                "Santa Catalina",
-                "Facultad de Informática",
-                "10:00",
-                "12-05-2023"
-            ),
+            Trip("Jane Smith", "Estación de autobuses", "Facultad de Ciencias", "09:00", "12-05-2023"),
+            Trip("Andres Rodriguez", "Santa Catalina", "Facultad de Informática", "10:00", "12-05-2023"),
             Trip("Nestor Maneiro", "Facultad de Ciencias", "Santa Catalina", "11:00", "12-05-2023"),
-            Trip(
-                "Juan Garcia",
-                "Facultad de Informática",
-                "Estación de autobuses",
-                "12:00",
-                "12-05-2023"
-            ),
+            Trip("Juan Garcia", "Facultad de Informática", "Estación de autobuses", "12:00", "12-05-2023"),
             Trip("Ana Fernandez", "Centro de la ciudad", "Universidad", "13:00", "12-05-2023"),
             Trip("Pedro Perez", "Facultad de Informática", "Santa Catalina", "14:00", "12-05-2023"),
-            Trip(
-                "Maria Lopez",
-                "Estación de autobuses",
-                "Facultad de Ciencias",
-                "15:00",
-                "12-05-2023"
-            ),
+            Trip("Maria Lopez", "Estación de autobuses", "Facultad de Ciencias", "15:00", "12-05-2023"),
             Trip("Jose Gonzalez", "Santa Catalina", "Centro de la ciudad", "16:00", "12-05-2023"),
-            Trip(
-                "Laura Sanchez",
-                "Facultad de Ciencias",
-                "Estación de autobuses",
-                "17:00",
-                "12-05-2023"
-            ),
+            Trip("Laura Sanchez", "Facultad de Ciencias", "Estación de autobuses", "17:00", "12-05-2023"),
+            Trip("Laura Sanchez", "Facultad de Ciencias", "Estación de autobuses", "17:00", "13-05-2023"),
+            Trip("Michael Brown", "Centro de la ciudad", "Santa Catalina", "08:30", "14-05-2023"),
+            Trip("Sophia Wilson", "Estación de autobuses", "Facultad de Ciencias", "10:30", "14-05-2023"),
+            Trip("Emma Davis", "Facultad de Informática", "Universidad", "12:30", "14-05-2023"),
+            Trip("Daniel Johnson", "Santa Catalina", "Centro de la ciudad", "14:30", "14-05-2023"),
+            Trip("Olivia Thompson", "Facultad de Ciencias", "Estación de autobuses", "16:30", "14-05-2023"),
+            Trip("William Martinez", "Universidad", "Facultad de Informática", "09:45", "15-05-2023"),
+            Trip("Ava Adams", "Estación de autobuses", "Santa Catalina", "11:45", "15-05-2023"),
+            Trip("James Thomas", "Centro de la ciudad", "Facultad de Ciencias", "13:45", "15-05-2023"),
+            Trip("Sophia Wilson", "Facultad de Informática", "Estación de autobuses", "15:45", "15-05-2023"),
+            Trip("Emily Taylor", "Santa Catalina", "Centro de la ciudad", "17:45", "15-05-2023"),
+            Trip("James Thomas", "Centro de la ciudad", "Facultad de Ciencias", "13:45", "17-05-2023"),
+            Trip("Sophia Wilson", "Facultad de Informática", "Estación de autobuses", "15:45", "17-05-2023"),
+            Trip("Emily Taylor", "Santa Catalina", "Centro de la ciudad", "17:45", "17-05-2023"),
+            Trip("Laura Sanchez", "Facultad de Ciencias", "Estación de autobuses", "17:00", "17-05-2023"),
+            Trip("Laura Sanchez", "Facultad de Ciencias", "Estación de autobuses", "16:00", "17-05-2023"),
+            Trip("Michael Brown", "Centro de la ciudad", "Santa Catalina", "08:30", "17-05-2023"),
+            Trip("Sophia Wilson", "Estación de autobuses", "Facultad de Ciencias", "10:30", "17-05-2023"),
+            Trip("Emma Davis", "Facultad de Informática", "Universidad", "12:30", "17-05-2023"),
+            Trip("Daniel Johnson", "Santa Catalina", "Centro de la ciudad", "14:30", "17-05-2023"),
+
         )
     }
+
 
     private fun showFilter() {
         if (filterContainer.childCount == 0) {
@@ -135,6 +135,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
     private fun resetFilter() {
         locationFilter.setText("")
         val today = LocalDate.now()
@@ -147,6 +148,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun applyFilter() {
+        if (!::datePickerFilter.isInitialized) {
+            // Inicializar datePickerFilter con fecha actual si aún no ha sido inicializado
+            val today = LocalDate.now()
+            datePickerFilter = DatePicker(this)
+            datePickerFilter.updateDate(today.year, today.monthValue - 1, today.dayOfMonth)
+        }
         val location = locationFilter.text.toString().lowercase().trim()
         val year = datePickerFilter.year
         val month = datePickerFilter.month
@@ -157,15 +164,15 @@ class MainActivity : AppCompatActivity() {
         val selectedDateTime = LocalDateTime.of(year, month + 1, day, hour, minute)
 
         val filteredTrips = tripsList.filter {
-            val tripDateTime = LocalDateTime.parse(
-                it.departureTime,
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+            val tripDateTime = LocalDateTime.of(
+                LocalDate.parse(it.departureDate, DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+                LocalTime.parse(it.departureTime, DateTimeFormatter.ofPattern("HH:mm"))
             )
 
-            val locationFilterApplied =
-                location.isNotBlank() && it.destinationLocation.lowercase().contains(location)
-            val timeFilterApplied =
-                Duration.between(selectedDateTime, tripDateTime).toMinutes().absoluteValue <= 120
+            val locationFilterApplied = location.isNotBlank() && it.destinationLocation.lowercase().contains(location.lowercase())
+
+            val timeDifference = Duration.between(selectedDateTime, tripDateTime).abs()
+            val timeFilterApplied = timeDifference <= Duration.ofHours(2)
 
             if (location.isNotBlank() && (hour != 0 || minute != 0)) {
                 locationFilterApplied && timeFilterApplied
@@ -177,6 +184,7 @@ class MainActivity : AppCompatActivity() {
                 true
             }
         }
+
 
         if (filteredTrips.isEmpty()) {
             Toast.makeText(
@@ -190,7 +198,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         recyclerView.adapter = tripAdapter
-        filterContainer.visibility = View.GONE // Cambia esta línea para ocultar el filter_container
+        filterContainer.visibility = View.GONE
     }
 
 
